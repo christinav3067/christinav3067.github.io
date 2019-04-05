df5554478921db8400aaa06dc0aaf6b1513a618e
abbeyk2001 <48104872+abbeyk2001@users.noreply.github.com>
2019-04-05 12:43:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.createElement("button");
+button.innerHTML = "Royal Flycathcer";
+    var body = document.getElementsByTagName("body")[0];
+body.appendChild(button); 
+  button.addEventListener ("click", function() {
+location.href = 'https://neotropical.birds.cornell.edu/Species-Account/nb/species/royfly1/overview'
