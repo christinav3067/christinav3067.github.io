ea5fbc6e26edc78842552da7a792409dd3b92a76
abbeyk2001 <48104872+abbeyk2001@users.noreply.github.com>
2019-04-08 13:24:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
- var button = document.createElement("button");
-button.innerHTML = "Just Click It";
-    var body = document.getElementsByTagName("body")[0];
-body.appendChild(button); 
-  button.addEventListener ("click", function() {
-location.href = 'https://neotropical.birds.cornell.edu/Species-Account/nb/species/royfly1/overview'
-  });
+
