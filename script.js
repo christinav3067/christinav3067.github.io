feb0ff01ef2f21f4886c649dc183380ace0a9679
abbeyk2001 <48104872+abbeyk2001@users.noreply.github.com>
2019-04-08 13:46:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function changeBackground(color) {
-   document.body.style.background = color;
+body{
+    background-color:black;
+    animation: image 10s infinite alternate;
+    animation:image 10s infinite alternate;
+    animation:image 10s infinite alternate;
 }
 
-window.addEventListener("load",function() { changeBackground('green'); });
+@keyframes image{
+    0%{
+background-color:blue;
+}
+25%/{
+    background-color:red;
+}
+50%{
+    background-color:green;
+}
+75%{
+
+    background-color:pink;
+}
+100%{
+    background-color:yellow;
+    }
+  }  
