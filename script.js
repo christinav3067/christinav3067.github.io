ecbbe317ed38347128bda989fcdda6be3d116ac9
abbeyk2001 <48104872+abbeyk2001@users.noreply.github.com>
2019-04-08 13:45:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ function changeBackground(color) {
    document.body.style.background = color;
 }
 
-window.addEventListener("load",function() { changeBackground('red'); });
+window.addEventListener("load",function() { changeBackground('green'); });
